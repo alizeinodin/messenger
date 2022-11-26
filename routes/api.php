15d5c4ba5ae6e889caf214cb77ed6b8bc77de602
alizeinodin <alizeinodin79@gmail.com>
2022-11-26 21:11:09 +0200

    login api add to api.php

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function () {
     Route::post('/register', 'register')
         ->name('register');
+    Route::post('/login', 'login')
+        ->name('login');
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
