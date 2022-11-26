f1ee80502920bfeb8be7d4c1bb961cdc7eb1e201
alizeinodin <alizeinodin79@gmail.com>
2022-11-26 13:38:38 +0330

    - add git to project - AuthController add - Request for Login and Register add - user table of database modify

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response as ResponseHTTP;
+
+class AuthController extends Controller
+{
+    /**
+     * @param RegisterRequest $request
+     * @return Response|Application|ResponseFactory
+     */
+    public function register(RegisterRequest $request): Response|Application|ResponseFactory
+    {
+        $cleanData = $request->validated();
+
+        $user = User::create([
+            'name' => $cleanData['name'],
+            'username' => $cleanData['username'],
+            'password' => Hash::make($cleanData['password']),
+        ]);
+
+        $res = [
+            'user' => $user,
+            'token' => $user->createToken('auth_token')->plainTextToken,
+        ];
+
+        return response($res, ResponseHTTP::HTTP_CREATED);
+    }
+
+    /**
+     * @param LoginRequest $request
+     * @return Response|Application|ResponseFactory
+     * @throws ValidationException
+     */
+    public function login(LoginRequest $request): Response|Application|ResponseFactory
+    {
+        $cleanData = $request->validated();
+
+        $user = User::where([
+            'username' => $cleanData['username']
+        ]);
+
+        if (Auth::attempt(['username' => $cleanData['username'], 'password' => $cleanData['password']])) {
+
+            $response = [
+                'user' => $user,
+                'access_token' => $user->createToken('auth_token')->plainTextToken,
+                'token_type' => 'Bearer',
+            ];
+
+            return response($response, ResponseHTTP::HTTP_OK);
+        }
+
+        throw ValidationException::withMessages([
+            'username' => ['The provided credentials are incorrect.']
+        ]);
+
+    }
+}
