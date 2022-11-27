b776ad4065f77dba3e97cb93b294230a9b46ee41
alizeinodin <alizeinodin79@gmail.com>
2022-11-28 01:16:08 +0200

    belongsTo add in Message Model for relationship

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the Sender that owns the Messages
+     *
+     * @return BelongsTo
+     */
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    /**
+     * Get the Receiver that owns the Messages
+     *
+     * @return BelongsTo
+     */
+    public function receiver(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
