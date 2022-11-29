b232a3772dffb638a878a62a31b35b8ba1398442
alizeinodin <alizeinodin79@gmail.com>
2022-11-29 11:23:42 +0200

    foriegnKey for 1-N relationship between sender and message and receiver and message add

--- database/migrations/2022_11_26_193750_create_messages_table.php
+++ database/migrations/2022_11_26_193750_create_messages_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,18 +14,22 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('sender_id')->unsigned(); // for one-to-many relationship
+            $table->bigInteger('receiver_id')->unsigned(); // for one-to-many relationship
             $table->longText('content');
             $table->timestamps();
 
             // define foreign key for sender
-            $table->foreignId('sender_id')
-                ->constrained('users')
+            $table->foreign('sender_id')
+                ->references('id')
+                ->on('users')
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
 
             // define foreign key for receiver
-            $table->foreignId('receiver_id')
-                ->constrained('users')
+            $table->foreign('receiver_id')
+                ->references('id')
+                ->on('users')
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
         });
