0df64f037bf548fd5d5b1b15fb0a29e160bb8c8f
alizeinodin <alizeinodin79@gmail.com>
2022-11-30 23:19:43 +0330

    getMessage method add

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,7 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class MessageController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function getMessages(Request $request)
+    {
+        $cleanData = $request->validated();
+
+        $messages = Message::where([
+            'sender_id' => $request->user(),
+            'receiver_id' => $cleanData['user_id'],
+        ])->all();
+
+        $response = [
+            'messages' => $messages,
+        ];
+
+        return response($response, ResponseAlias::HTTP_OK);
+    }
 }
