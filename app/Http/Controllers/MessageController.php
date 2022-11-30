a5bf9d2dd1ac56e24b802b48c28063f953d4011e
alizeinodin <alizeinodin79@gmail.com>
2022-11-30 23:27:35 +0330

    sendMessage method add

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Message;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class MessageController extends Controller
 {
     /**
      * @param Request $request
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     * @return Application|ResponseFactory|Response
      */
     public function getMessages(Request $request)
     {
@@ -26,5 +29,27 @@ class MessageController extends Controller
         ];
 
         return response($response, ResponseAlias::HTTP_OK);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response|Application|ResponseFactory
+     */
+    public function sendMessage(Request $request): Response|Application|ResponseFactory
+    {
+        $cleanData = $request->validated();
+
+        $message = Message::create([
+            'content' => $cleanData['content'],
+            'sender_id' => $request->user()->id,
+            'receiver_id' => $cleanData['receiver_id'],
+        ]);
+
+        $response = [
+            'isOk' => 'true',
+            'message' => $message
+        ];
+
+        return response($response, ResponseAlias::HTTP_CREATED);
     }
 }
