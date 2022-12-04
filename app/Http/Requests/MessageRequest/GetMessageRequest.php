fc7e691bb0dd29f74c7e54fc03a515f4f50046b8
alizeinodin <alizeinodin79@gmail.com>
2022-12-04 17:17:53 +0330

    GetMessageRequest develop

--- app/Http/Requests/MessageRequest/GetMessageRequest.php
+++ app/Http/Requests/MessageRequest/GetMessageRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\MessageRequest;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class getMessageRequest extends FormRequest
+class GetMessageRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class getMessageRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class getMessageRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'receiver_id' => 'required|exists:users,id',
         ];
     }
 }
