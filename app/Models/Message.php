01117608eab1f2c129be56bb354e45385fa187b6
alizeinodin <alizeinodin79@gmail.com>
2022-12-06 23:56:33 +0330

    position attribute add in message

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -5,14 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class Message extends Model
 {
     use HasFactory;
 
+    protected const POSITION_LEFT = "left";
+    protected const POSITION_RIGHT = "right";
+
     protected $fillable = [
         'content'
     ];
+
+    protected $appends = [
+        'position'
+    ];
+
+    /**
+     * Get position of a message in the chat
+     *
+     * @return string
+     */
+    public function getPositionAttribute(): string
+    {
+        return $this->sender_id === Auth::user()->id ? $this::POSITION_LEFT : $this::POSITION_RIGHT;
+    }
 
     /**
      * Get the Sender that owns the Messages
