70f1f4ddafb96c90ec9a8d93e355087a80950d00
alizeinodin <alizeinodin79@gmail.com>
2022-12-07 17:54:17 +0330

    find method add in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class UserController extends Controller
 {
-    //
+    public function find(Request $request)
+    {
+        $cleanData = $request->validated();
+
+        $users = User::where([
+            'username' => '%' . $cleanData['username'] . '%',
+        ])->get();
+
+        $response = [
+            'result' => $users,
+            'isOk' => 'true',
+        ];
+
+        return response($response, ResponseAlias::HTTP_OK);
+    }
 }
