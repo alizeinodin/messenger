9170c2b3bff1b9f51b850db703eea22374ef4e82
alizeinodin <alizeinodin79@gmail.com>
2022-12-07 15:22:46 +0000

    createUser method add, test get user fix

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,21 +2,29 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
+
+    private function createUser()
+    {
+        return User::factory()->create();
+    }
     /**
-     * A basic feature test example.
-     *
+     * get user of request in app
      * @return void
      */
-    public function test_example()
+    public function test_get_user(): void
     {
-        $response = $this->get('/');
+        $user = $this->createUser();
+        $this->actingAs($user);
 
-        $response->assertStatus(200);
+        $response = $this->getJson(route('user.get'));
+        $response->assertOk();
     }
 }
