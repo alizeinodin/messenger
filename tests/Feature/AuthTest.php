814dedc6c121c11247728b0de53487b03b1db870
alizeinodin <alizeinodin79@gmail.com>
2023-01-01 12:42:08 +0330

    createUser method add to AuthTest

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,12 +2,18 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
+    private function createUser()
+    {
+        return User::factory()->create();
+    }
+
     use RefreshDatabase;
 
     /**
