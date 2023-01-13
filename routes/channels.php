6b5e20f93cdf5121a66bd82ee086fc670ece4055
alizeinodin <alizeinodin79@gmail.com>
2023-01-13 17:17:04 +0330

    chat broadcast add in channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int)$user->id === (int)$id;
+});
+
+Broadcast::channel('chat{id}', function ($id, $user) {
+    return \Illuminate\Support\Facades\Auth::check();
 });
